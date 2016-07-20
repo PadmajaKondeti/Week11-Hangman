7424de5091c171eb7c2ee12f245b0ddd5d660641
pkondeti <padma.kondeti@gmail.com>
2016-07-20 15:23:06 -0400

    letter guessing works for single letter

--- letter.js
+++ letter.js
@@ -1,47 +1,28 @@
 var inquirer = require('inquirer');
 var word = require('./word.js');
-// count++;
-		// playerList();
-	//
-//
 console.log(word);
 var count = 0;
-var wordBlank="";
+var wordBlank=[];
 var length1 = word.length;
 for (var i=0; i<length1; i++){
-	wordBlank +=" _ " ;
+	wordBlank[i] =" _ ";
 }
 console.log(wordBlank);
-// var letter = function(){
-//     if (count < length1+2 ) {
-// 		//RUNS INQUIRER AND ASKS THE USER A SERIES OF QUESTIONS WHOSE REPLIES ARE STORED WITHIN THE VARIABLE answers INSIDE OF THE .then STATEMENT.
-// 		inquirer.prompt([{	
-// 			name: "guess",
-// 			message: "Guess a letter?"
-// 			}
-// 		]).then(function(answer) {
-// 			console.log(answer);
-			
-// 		})
-//         count++;
-//         letter();
-//     };
-// };
-// letter();
 var letter1 = function(){
-   // if (count < length1+2 ) {
+    if (count < length1) {
 		//RUNS INQUIRER AND ASKS THE USER A SERIES OF QUESTIONS WHOSE REPLIES ARE STORED WITHIN THE VARIABLE answers INSIDE OF THE .then STATEMENT.
 		inquirer.prompt([{	
 			name: "guess",
 			message: "Guess a letter?"
 			}
 		]).then(function(answer) {
-			var value = word.indexOf(answer);
+			var value = word.indexOf(answer.guess);
 			console.log(value);
-			
+			wordBlank[value] = " "+ answer.guess + " ";
+			console.log(wordBlank);
 		})
-	   // count++;
-	   // letter();
+	   count++;
+	   //letter1();
     };
-
+};
 letter1();
