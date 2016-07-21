5ee6b3c77aaee72f84c71e3e4b0093e28c19bf37
pkondeti <padma.kondeti@gmail.com>
2016-07-21 16:23:32 -0400

    working version for one letter guess

--- word.js
+++ word.js
@@ -1,12 +1,6 @@
-// var letter = require('./letter.js');
-// const randomWord = require('random-word');
-// var guessWord = randomWord();
-// module.exports = randomWord();
 var fs = require('fs');
- 
 // returns the path to the word list which is separated by `\n` 
 var wordListPath = require('word-list');
- 
 var wordArray = fs.readFileSync(wordListPath, 'utf8')
 var wordArray = wordArray.split('\n');
 
