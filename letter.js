26e41979d14126d8c7ef66ad3487221001682d18
pkondeti <padma.kondeti@gmail.com>
2016-07-21 16:17:42 -0400

    working version for one letter guess

--- letter.js
+++ letter.js
@@ -3,14 +3,11 @@ var word = require('./word.js');
 console.log(word);
 var count = 0;
 var wordBlank=[];
-var length1 = word.length;
-for (var i=0; i<length1; i++){
+var wordlength = word.length;
+for (var i=0; i<wordlength; i++){
 	wordBlank += "_ ";
-}
-
-wordBlank = wordBlank.trim().split(' ');
-
-console.log(wordBlank);
+};
+var wordBlankArray = wordBlank.trim().split(' ');
 var guessLetter = function(){
 	inquirer.prompt([{	
 		name: "guess",
@@ -18,36 +15,24 @@ var guessLetter = function(){
 		}
 	]).then(function(answer) {
 		var indices = [];
-		for(var i=0; i<word.length;i++) {
+		for(var i=0; i<wordlength;i++) {
 			if (word[i] == answer.guess) indices.push(i);
 		}
-		
-		
 		var value = word.indexOf(answer.guess);
 		if (indices.length > 0 ){
 			for(var i=0; i<indices.length;i++) {
-				wordBlank[indices[i]] = answer.guess ;
+				wordBlankArray[indices[i]] = answer.guess ;
 			}
-			console.log(value);
-			
-			wordBlank[value]= answer.guess;
-
-			console.log((wordBlank.toString()).replace(/,/g, ' '));
-			
+			console.log((wordBlankArray.toString()).replace(/,/g, ' '));
 		}		
 	})
 };
-//console.log(wordBlank);
-//wordBlank = wordBlank.split("");
-var letter1 = function(){
-	if (count < 1) {
-		//RUNS INQUIRER AND ASKS THE USER A SERIES OF QUESTIONS WHOSE REPLIES ARE STORED WITHIN THE VARIABLE answers INSIDE OF THE .then STATEMENT.
+var wordGame = function(){
+			
+	if (count < wordlength){
 		guessLetter();
-		
-	   count++;
-	   letter1();
-    } else {
-    	return false;
-    };
+		count++;
+	};
+	//wordGame();
 };
-letter1();
+wordGame();
