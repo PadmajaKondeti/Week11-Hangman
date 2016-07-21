5d1f04f28327b43e21ae36055616120e067d8442
pkondeti <padma.kondeti@gmail.com>
2016-07-21 15:07:58 -0400

    working one blank replacement with a letter

--- letter.js
+++ letter.js
@@ -5,28 +5,44 @@ var count = 0;
 var wordBlank=[];
 var length1 = word.length;
 for (var i=0; i<length1; i++){
-	wordBlank +=" _ ";
+	wordBlank += "_ ";
 }
-require('events').EventEmitter.prototype._maxListeners = 100;
+
+wordBlank = wordBlank.trim().split(' ');
+
 console.log(wordBlank);
-wordBlank = wordBlank.split("");
+var guessLetter = function(){
+	inquirer.prompt([{	
+		name: "guess",
+		message: "Guess a letter?"
+		}
+	]).then(function(answer) {
+
+		
+		var value = word.indexOf(answer.guess);
+		if (value >= 0 ){
+			
+			console.log(value);
+			
+			wordBlank[value]= answer.guess;
+
+			console.log((wordBlank.toString()).replace(/,/g, ' '));
+			
+		}		
+	})
+};
+require('events').EventEmitter.prototype._maxListeners = 100;
+//console.log(wordBlank);
+//wordBlank = wordBlank.split("");
 var letter1 = function(){
-	if (count < length1) {
+	if (count < 1) {
 		//RUNS INQUIRER AND ASKS THE USER A SERIES OF QUESTIONS WHOSE REPLIES ARE STORED WITHIN THE VARIABLE answers INSIDE OF THE .then STATEMENT.
-		inquirer.prompt([{	
-			name: "guess",
-			message: "Guess a letter?"
-			}
-		]).then(function(answer) {
-			var value = word.indexOf(answer.guess);
-			if (value >= 0 ){
-				console.log(value);
-				wordBlank[value] = " "+ answer.guess + " ";
-				console.log(wordBlank.toString());
-			}		
-		})
+		guessLetter();
+		
 	   count++;
 	   letter1();
+    } else {
+    	return false;
     };
 };
 letter1();
